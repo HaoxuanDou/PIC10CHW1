f6e7e0eb3d6dc64aa61b6d6429c8c67b5c2f483a
Haoxuan Dou <douhaoxuan@gmail.com>
2016-10-23 11:33:23 -0700

    Experimenting with .cpp file

--- Desktop/pic10c/assignment1/siete_y_medio.cpp
+++ Desktop/pic10c/assignment1/siete_y_medio.cpp
@@ -11,13 +11,27 @@ using namespace std;
 
 
 // Non member functions declarations (if any)
-
+void oneRound(Player& player, Hand& playerhand, Hand& dealerHand){
+	cout << "You have $" << player.getMoney() << ". Enter bet: ";
+    int currentBet;
+    cin >> currentBet;
+    player.makeBet(currentBet);
+    cout << "Your cards"
+}
 
 // Non member functions implementations (if any)
 
 
 // Stub for main
 int main(){
-   /* --STATEMENTS-- */
+   Player player(100);
+   Hand playerHand;
+   Hand dealerHand;
+   
+  
+   
+   
+   
+   
    return 0;
 }
